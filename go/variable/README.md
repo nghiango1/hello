bafa6146ee9d3c291edd5328225764530ea97c86
termux <ylsama>
2023-09-08 09:27:22 +0700

    adding variable - Linked list

--- go/variable/README.md
+++ go/variable/README.md
@@ -6,13 +6,21 @@ go mod init example/variable
 
 ## Init a variable
 
-You start with `var`
+You start with `var` to decleare a variable
 
 ```go
-var message string
+var message string = "Hello world!"
 //          ^^^^^^--- Type
 //  ^^^^^^^----- Name
 ```
+
+Or in compact syntax with `:=`, which get type automatically
+
+```go
+message := "Hello world!"
+```
+
+> Also, we use `//` to start a comment (Unused code that ignore by the compiler)
 
 This can either in the scope of function/main funtion or even outside
 Start with this example file
@@ -30,3 +38,57 @@ func main() {
      fmt.Println(message)
 }
 ```
+
+## Data type
+
+Number integer: `int` default 64 bit
+
+```go
+var i int 
+
+func main() {
+    fmt.Println(unsafe.Sizeof(i))
+}
+```
+
+String: Above
+
+## Structure type
+
+### Linked list, Pointer
+
+Start new type with `type`
+
+```
+## Check the variable.go code
+```
+
+- We can declare in main function, other function can access main function variable (??)
+
+> This mean, index variable can be change inside a function, affecting the main function loop process
+> Proccing a Bug (!!)
+
+- We can decleare function after it being call (??)
+- New object can be create using a normal `var`, or can be specifically create with pointer like this
+
+    ```go
+        var newNode *Node = &Node{v, nil};
+    ```
+
+- Pointer (Pretty much like C):
+    - `*p` : Accessing true value
+    - `&p` : Passing address
+    - `var p *Node` : Decleare pointer
+
+- For loop only (I can't even find While loop with cheat sheet `cht.sh`, it said go not have while loop)
+    - `for i:=1; i < 10; i ++ {}`, remember to have `:=` for the index value, or you can accessing others parrent function 
+    - `nil` check with `==`, same as c:
+    ```go
+        if config == nil {
+            // then
+        }
+    ```
+- Any class in go lang tranfer me go a **go way** of doing thing, [sample website](https://tip.golang.org/doc/effective_go).
+    - Very little of OOP, this languagle isn't Java. 
+
+Overal take, it a very generest programming language on function (int auto 64 bit, time also bigger, println accepting a lot more input, no need to free anything, ??variable in heap - not stack so it available in many scope??)
--- go/variable/variable.go
+++ go/variable/variable.go
@@ -1,12 +1,124 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+ 	"math/rand"
+    "time"
+)
 
 var message string
+var i int // This default to be 8 bytes
+var i2 int8
+var i3 int16
+var i4 int32
+var i5 int64
+
+type Node struct {
+    value int
+    next *Node
+}
+
+type List struct {
+    head *Node
+    tail *Node
+    length int
+}
+
+func initList(l *List) {
+    l.length = 0
+    l.head = nil
+    l.tail = nil
+}
+
+func push(l *List, v int) {
+    l.length += 1;
+    var newNode *Node = &Node{v, nil};
+    if l.head == nil {
+        l.head = newNode;
+        l.tail = l.head;
+    } else {
+        l.tail.next = newNode;
+        l.tail = l.tail.next;
+    };
+}
+
+func printListCompact(l *List) {
+    fmt.Printf("List length = %d", l.length)
+    fmt.Println()
+}
+
+func printList(l *List) {
+    var p *Node
+    p = l.head
+
+    fmt.Printf("List length = %d, with element:", l.length)
+    for i:= 1; i < l.length; i ++ {
+        fmt.Printf(" %d", p.value)
+        if p != nil { p = p.next }
+    }
+    fmt.Println()
+}
 
 func main() {
-    // Test
+    // Test commending
     message = "Hello world!"
     fmt.Println(message)
+    fmt.Println(unsafe.Sizeof(i))
+    fmt.Println(unsafe.Sizeof(i2))
+    fmt.Println(unsafe.Sizeof(i3))
+    fmt.Println(unsafe.Sizeof(i4))
+    fmt.Println(unsafe.Sizeof(i5))
+
+    var list List
+    initList(&list)
+
+    var multi int = 10_000
+    var length int = 100*multi
+    var total int = 5*multi
+    var first int = 50*multi
+    var i int
+
+    for i = 0; i < length; i++ {
+        push(&list, rand.Intn(1000000))
+    }
+
+    printListCompact(&list)
+
+    now := time.Now()
+    for i = 0; i < total; i++ {
+        var pos int = rand.Intn(first)
+        deleteList(&list, pos)
+        if i % 1000 == 0 {
+            fmt.Printf("%d ", i / 1000)
+        }
+    }
+
+    fmt.Println()
+
+    fmt.Println( (- now.UnixMilli() + time.Now().UnixMilli()) / 1000)
+    printListCompact(&list)
+}
+
+func deleteList(l *List, pos int) {
+    var p *Node
+    var i int
+
+    l.length --
+    p = l.head
+    if pos == 0 {
+        l.head = l.head.next
+        if l.head == nil {
+            l.tail = nil
+        }
+        return
+    }
+
+    for i = 1; i < pos; i ++ {
+        p = p.next
+    }
+
+    var nextP *Node = p.next
+    p.next = nextP.next
 }
 
