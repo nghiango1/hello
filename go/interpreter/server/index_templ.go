df9f8515fcb109e0f565bba581c08ab25ec6ea3e
nghiango1 <ducnghia.tin47@gmail.com>
2024-02-28 01:34:09 +0700

    Rebuild template file

--- go/interpreter/server/index_templ.go
+++ go/interpreter/server/index_templ.go
@@ -339,7 +339,7 @@ func exampleFragment() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = exampleCodeComponent("Boolean", "True").Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = exampleCodeComponent("Boolean", "true").Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
