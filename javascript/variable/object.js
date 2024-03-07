f64e1e0da6aea2256aac7337487d0c433e36d7ca
nghiango1 <ducnghia.tin47@gmail.com>
2024-03-07 16:34:27 +0700

    variable done

--- javascript/variable/object.js
+++ javascript/variable/object.js
@@ -1,79 +1,127 @@
+import { println } from "./function.mjs";
+
 const one = "one";
 const a = 1;
 
-const count = {
-    // Key with the same value
-    a,
-    // Function
-    /**
-     * @returns {number} return 2
-     */
-    b: function aDifferentName() { return 2; },
-    /**
-     * @returns {number} return 3
-     */
-    c() { return 3; },
-    // Bad
-    /* eslint object-shorthand: off */
-    one: one,
-    /**
-     * @returns {string} return two
-     */
-    two: function two() { return "two"; },
-    /**
-     * @returns {string} return three
-     */
-    three: function() { return "three"; },
-    // Good
-};
+{
+    const count = {
+        // Key with the same value
+        a,
+        // Function
+        /** @returns {number} return 2 */
+        b() { return 2; },
+        /** @returns {number} return 3 */
+        c: function aDifferentName() { return 3; },
+        // Bad
+        /* eslint object-shorthand: off */
+        one: one,
+        /** @returns {string} return two */
+        two: function two() { return "two"; },
+        /** @returns {string} return three */
+        three: function() { return "three"; },
+    };
 
-console.log(count);
+    console.log(count);
 
-// Check if object have a property
-const have = Object.prototype.hasOwnProperty.call(count, "c");
-if (have) console.log("count object have c property");
+    // Check if object have a property
+    const have = Object.prototype.hasOwnProperty.call(count, "c");
+    if (have) console.log("count object have c property");
 
 
-// Bad-way with example - hasOwnProperty can be overiden, could also be not
-// initilized in object(null) which has null prototype. So the result of 
-// <object>.hasOwnProperty() is unreliable.
-/* eslint no-prototype-builtins: off */
+    // Bad-way with example - hasOwnProperty can be overiden, could also be not
+    // initilized in object(null) which has null prototype. So the result of 
+    // <object>.hasOwnProperty() is unreliable.
 
-// true - count do have c in it property
-console.log(count.hasOwnProperty("c"));
+    /* eslint no-prototype-builtins: off */
 
-// false - count don't have d in it property
-console.log(count.hasOwnProperty("d"));
+    // true - count do have c in it property
+    console.log(count.hasOwnProperty("c"));
+
+    // false - count don't have d in it property
+    console.log(count.hasOwnProperty("d"));
 
 
-const nullobj = Object.create(null);
-// false - nullobj have null prototype, so it not have 'c' property
-console.log(Object.prototype.hasOwnProperty.call(nullobj, "c"));
+    const nullobj = Object.create(null);
+    // false - nullobj have null prototype, so it not have 'c' property
+    console.log(Object.prototype.hasOwnProperty.call(nullobj, "c"));
 
-// error - nullobj have null prototype, so it not have 'hasOwnProperty'
-// function property. And try to access a null cause error throw
-try {
-    // Error - object(null) have null prototype, which will throw error if we
-    // try to access its prototype
-    console.log(nullobj.hasOwnProperty("c"));
-} catch (error) {
-    console.log("This throw error: ", error);
+    // error - nullobj have null prototype, so it not have 'hasOwnProperty'
+    // function property. And try to access a null cause error throw
+    try {
+        // Error - object(null) have null prototype, which will throw error if we
+        // try to access its prototype
+        console.log(nullobj.hasOwnProperty("c"));
+    } catch (error) {
+        console.log("This throw error: ", error);
+    }
+
+    // hasOwnProperty here is overiden
+    const notcount = {
+        /**
+         * Echo back any string
+         * @param {string} x any thing param
+         * @returns {string} echo it back
+         */
+        hasOwnProperty(x) { return x; },
+        c: "c",
+    };
+
+    // true - notcount have c prototype
+    console.log(Object.prototype.hasOwnProperty.call(notcount, "c"));
+
+    // "c" - notcount have hasOwnProperty prototype, which is a function that echo
+    // the param back to us
+    console.log(notcount.hasOwnProperty("c"));
 }
 
-// hasOwnProperty here is overiden
-const notcount = {
-    /**
-     * Echo back any string
-     * @param {string} x any thing param
-     * @returns {string} echo it back
-     */
-    hasOwnProperty(x) { return x; },
-    c: "c",
-};
+// Copy object property with object spread (introduced in ES2018)
+{
+    const original = { a: 1, b: 2 };
+    const copy = { ...original, c: 3 };
+    println(copy);
 
-// true - notcount have c prototype
-console.log(Object.prototype.hasOwnProperty.call(notcount, "c"));
+    const { a, ...noA } = copy;
+    println("Do some thing with", a);
+    println("Rest are:", noA);
+}
 
-// "c" - notcount have hasOwnProperty prototype, which is a function that echo
-// the param back to us
-console.log(notcount.hasOwnProperty("c"));
+// Copy object property with object.assign (used in ES2018 and bellow)
+{
+    // very bad - wrong way of useage Object.assign(target, source)
+    {
+        /** @type {{a?: number, b:number}}*/
+        const original = { a: 1, b: 2 };
+        const copy = Object.assign(original, { c: 3 }); // this mutates `original` (the target) and return it reference back to copy
+        println("cpy-verybad", copy);
+        println("ori-verybad", original);
+        delete copy.a; // so does this
+        println("cpy-verybad", copy);
+        println("ori-vertbad", original);
+    }
+
+    // bad - Object.assign right usage with two args
+    {
+        /** @type {{a?: number, b:number}}*/
+        const original = { a: 1, b: 2 };
+        const copy = Object.assign({ c: 3 }, original); // this mutates `original` (the target) and return it reference back to copy
+        println("cpy-bad", copy);
+        println("ori-bad", original);
+        delete copy.a; 
+        println("cpy-bad", copy);
+        println("ori-bad", original);
+    }
+    // bad - difference way of useage when three and more arg
+    {
+        /** @type {{a?: number, b:number}}*/
+        const original = { a: 1, b: 2 };
+
+        /* eslint prefer-object-spread: off */
+
+        // Eslint rule can convert this to ES2018 syntax
+        const copy = Object.assign({}, original, { c: 3 }, { d: 4 });
+        println("cpy", copy);
+        delete copy.a; 
+        println("cpy", copy);
+        println("ori", original);
+    }
+}
--- javascript/variable/variable.js
+++ javascript/variable/variable.js
@@ -84,20 +84,31 @@ println(`\nhello is out of scope so it now back to type = '${typeof hello}'`);
 }
 
 // Scope - This break a lot of safety from LSP, eslint rule, so just stick with let and const
-// Read more on scope.js
+{
+    // Read more on scope.js
+}
 
 // Object - With some Airbnb js style guilde
 {
     println("\nObject type advanced\n===========\n");
-    // Also, alway using const for reference variable
-    // Key name generate function
+    // Alway using const for reference variable so we can't reassign reference
+    // which reduce bug and make debug easier
+    const objOne = { a: "a" };
+    objOne.a = "b";
+    println(objOne);
+
+    let objTwo = objOne;
+    // This reference to a newly created object
+    objTwo = { a: "a" };
+    println(objTwo);
+
     /**
      * Generate a new key name with a salt value
-     * @param {string} key salt value
+     * @param {string} salt salt string
      * @returns {string} generated key
      */
-    const newName = function constructKeyNameForObject(key) {
-        return `new_${key}`;
+    const newName = function constructKeyNameForObject(salt) {
+        return `new_${salt}`;
     };
 
     // Init with a generated field name using a function
@@ -161,6 +172,16 @@ println(`\nhello is out of scope so it now back to type = '${typeof hello}'`);
     println(Object.prototype.hasOwnProperty.call(count, "c"));
 
     // Check more in object.js
+    // Copy object property with object spread (introduced in ES2018)
+    {
+        const original = { a: 1, b: 2 };
+        const copy = { ...original, c: 3 };
+        println(copy);
+
+        const { a, ...noA } = copy;
+        println("Do some thing with", a);
+        println("Rest are:", noA);
+    }
 }
 
 {
@@ -329,16 +350,12 @@ println(`\nhello is out of scope so it now back to type = '${typeof hello}'`);
      * @implements {Iterable<number>}
      */
     class customIterableClass {
-        /**
-         *
-         */
+        /** */
         constructor(counter = 0) {
             this.counter = counter;
         }
 
-        /**
-         *
-         */
+        /** */
         [Symbol.iterator]() {
             // This is a function of iterableObject, so we can access its value
             // from `this` keyword
@@ -347,9 +364,7 @@ println(`\nhello is out of scope so it now back to type = '${typeof hello}'`);
 
             /** @type { Iterator<number> } */
             const interatorObject = {
-                /**
-                 *
-                 */
+                /** */
                 next() {
                     if (upper.counter < 10) {
                         upper.counter += 1;
@@ -367,9 +382,7 @@ println(`\nhello is out of scope so it now back to type = '${typeof hello}'`);
                     return iteratorResult;
                 },
 
-                /**
-                 *
-                 */
+                /** */
                 return(value) {
                     console.log("Do some thing with", value);
                     console.log("Done interating, cleanning up");
@@ -381,9 +394,7 @@ println(`\nhello is out of scope so it now back to type = '${typeof hello}'`);
                     return iteratorResult;
                 },
 
-                /**
-                 *
-                 */
+                /** */
                 throw(exception) {
                     console.log("Do some thing with", exception);
                     console.log("Done interating, cleanning up");
@@ -419,15 +430,11 @@ println(`\nhello is out of scope so it now back to type = '${typeof hello}'`);
      * @property {number} done what
      */
 
-    /**
-     * @implements {Base}
-     */
+    /** @implements {Base} */
     class custom {
         what = 1;
         who = 1;
-        /**
-         * Constructor
-         */
+        /** Constructor */
         constructor() {
             this.done = 2;
         }
@@ -436,173 +443,3 @@ println(`\nhello is out of scope so it now back to type = '${typeof hello}'`);
     const a = new custom();
     println(a);
 }
-
-
-// type Node struct {
-//     value int
-//     next *Node
-// }
-//
-// type LinkedList struct {
-//     head *Node
-//     tail *Node
-//     length int
-// }
-//
-// func initLinkedList(l *LinkedList) {
-//     l.length = 0
-//     l.head = nil
-//     l.tail = nil
-// }
-//
-// func pushLinkedList(l *LinkedList, v int) {
-//     l.length += 1;
-//     var newNode *Node = &Node{v, nil};
-//     if l.head == nil {
-//         l.head = newNode;
-//         l.tail = l.head;
-//     } else {
-//         l.tail.next = newNode;
-//         l.tail = l.tail.next;
-//     };
-// }
-//
-// func printLinkedListCompact(l *LinkedList) {
-//     fmt.Printf("List length = %d", l.length)
-//     fmt.Println()
-// }
-//
-// func printLinkedList(l *LinkedList) {
-//     var p *Node
-//     p = l.head
-//
-//     fmt.Printf("List length = %d, with element:", l.length)
-//     for i:= 0; i < l.length; i ++ {
-//         fmt.Printf(" %d", p.value)
-//         if p != nil { p = p.next }
-//     }
-//     fmt.Println()
-// }
-//
-// type ArrayList struct {
-//     value [1_000_000]int32
-//     length int
-// }
-//
-// func initArrayList(l *ArrayList) {
-//     l.length = 0
-// }
-//
-// func pushArrayList(l *ArrayList, v int32) {
-//     l.value[l.length] = v
-//     l.length += 1;
-// }
-//
-// func printArrayListCompact(l *ArrayList) {
-//     fmt.Printf("List length = %d", l.length)
-//     fmt.Println()
-// }
-//
-// func printArrayList(l *ArrayList) {
-//     fmt.Printf("List length = %d, with element:", l.length)
-//     for i:= 0; i < l.length; i ++ {
-//         fmt.Printf(" %d", l.value[i])
-//     }
-//     fmt.Println()
-// }
-//
-// func main() {
-//     // Test commending
-//     message = "Hello world!"
-//     fmt.Println(message, len(message))
-//     fmt.Println(unsafe.Sizeof(i))
-//     fmt.Println(unsafe.Sizeof(i2))
-//     fmt.Println(unsafe.Sizeof(i3))
-//     fmt.Println(unsafe.Sizeof(i4))
-//     fmt.Println(unsafe.Sizeof(i5))
-//
-//     var multi int = 10_000
-//     var length int = 100*multi
-//     var total int = 5*multi
-//     var first int = 50*multi
-//
-//     var list LinkedList
-//     initLinkedList(&list)
-//     testLinkedList(length, list, total, first)
-//
-//   var list2 ArrayList
-//     initArrayList(&list2)
-//     testArrayList(length, list2, total, first)
-// }
-//
-// func testArrayList(length int, list2 ArrayList, total int, first int) {
-//   for i := 0; i < length; i++ {
-//     pushArrayList(&list2, int32( rand.Intn(1000000)))
-//   }
-//
-//   printArrayListCompact(&list2)
-//
-//   var now = time.Now()
-//   for i := 0; i < total; i++ {
-//     var pos int = rand.Intn(first)
-//     deleteArrayList(&list2, pos)
-//     if i%1000 == 0 {
-//       fmt.Printf("%d ", i/1000)
-//     }
-//   }
-//
-//   fmt.Println()
-//
-//   fmt.Println("ArrayList test done in: ", (-now.UnixMilli()+time.Now().UnixMilli())/1000, "s")
-//   printArrayListCompact(&list2)
-// }
-//
-// func deleteArrayList(l *ArrayList, pos int) {
-//     l.length --
-//
-//     for i = pos; i < l.length; i ++ {
-//         l.value[i] = l.value[i+1]
-//     }
-// }
-//
-// func testLinkedList(length int, list LinkedList, total int, first int) {
-//   for i := 0; i < length; i++ {
-//     pushLinkedList(&list, rand.Intn(1000000))
-//   }
-//
-//   printLinkedListCompact(&list)
-//
-//   var now = time.Now()
-//   for i := 0; i < total; i++ {
-//     var pos int = rand.Intn(first)
-//     deleteLinkedList(&list, pos)
-//     if i%1000 == 0 {
-//       fmt.Printf("%d ", i/1000)
-//     }
-//   }
-//
-//   fmt.Println()
-//   fmt.Println("LinkedList test done in: ", (-now.UnixMilli()+time.Now().UnixMilli())/1000, "s")
-// }
-//
-// func deleteLinkedList(l *LinkedList, pos int) {
-//     var p *Node
-//     var i int
-//
-//     l.length --
-//     p = l.head
-//     if pos == 0 {
-//         l.head = l.head.next
-//         if l.head == nil {
-//             l.tail = nil
-//         }
-//         return
-//     }
-//
-//     for i = 1; i < pos; i ++ {
-//         p = p.next
-//     }
-//
-//     var nextP *Node = p.next
-//     p.next = nextP.next
-// }
