b0f384761fc3b684138143db2192c3626977e63e
ylong <ducnghia.tin47@gmail.com>
2024-10-19 07:45:28 +0700

    better check for int32 limit bound

--- c/variable/int.c
+++ c/variable/int.c
@@ -9,6 +9,30 @@ int INT32_BOUND_MAX = (int)~0 ^ (1 << 31);
 // of int: INT32_BOUND_MIN = b100..0
 int INT32_BOUND_MIN = (int)0 ^ (1 << 31);
 
+/**
+ * @brief This comparation use minus tatic to keep thing within uint32_t bound
+ *
+ * @param target The number before adding x to the back
+ * @param x Will be add to the back of target
+ * @param bound Limit bound which needed to be check
+ * @return 0 (FALSE) when target..x won't goes above the bound
+ * @return 1 (TRUE) when target..x will goes above the bound
+ */
+int compare(uint32_t target, uint32_t x, uint32_t bound) {
+  uint32_t extra = (target % 10 + x) / 10;
+
+  if (target > bound / 10 - extra)
+    return 1;
+
+  if (target == bound / 10 - extra) {
+    uint32_t lastNumber = (x + target % 10) % 10;
+    if (lastNumber > bound % 10)
+      return 1;
+  }
+
+  return 0;
+}
+
 int reverse(int x) {
   uint32_t unsign_x = -0;
   int sign = 1;
@@ -24,7 +48,7 @@ int reverse(int x) {
 
   uint32_t res = 0;
   while (unsign_x > 0) {
-    if (res * 10 > bound - unsign_x % 10)
+    if (compare(res, unsign_x % 10, bound))
       return 0;
     res = res * 10 + unsign_x % 10;
     unsign_x = unsign_x / 10;
