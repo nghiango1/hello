3e507919505d725a084753d6d44182fe568945c4
ylong <ducnghia.tin47@gmail.com>
2024-10-23 22:51:02 +0700

    string operation

---
+++ ruby/string/operation-on-string.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+# frozen_string_literal: true
+
+# A easier life like Go/Python/...
+double = 'repeat' * 2
+puts double
+
+greetting = 'hello'
+add = greetting + 'world'
+puts add
+
+# Extracting character
+first = 'hello'[0]
+puts first
+
+# Reverted index
+last = 'hello'[-1]
+puts last
+
+# String compare
+puts greetting == 'hello'
